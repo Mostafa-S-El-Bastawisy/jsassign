5b2b2177f34813271797f12e7a26c41edfb45900
Mostafa S. El-bastawisy <84564484+Mostafa-S-El-Bastawisy@users.noreply.github.com>
2021-07-27 20:58:55 +0200

    Add file of assignment 023 to 026

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(100000 + 0); // 100000
+console.log(100001 - 1); // 100000
+console.log(100000 / 1); // 100000
+console.log(10000 * 10); // 100000
+console.log(100000 ** 1); // 100000
+console.log(100000 * 1); // 100000
+console.log(1000000 / 10); // 100000
+console.log(100000 % 10000000); // 100000
+
+console.log(min_save_integr()); // 9007199254740991
